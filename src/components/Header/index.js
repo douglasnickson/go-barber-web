05a01bc564a45254af651ffa2b3bf45c7aecd72f
Douglas Nickson <douglas.nickson1@gmail.com>
2020-09-07 20:18:57 -0300

    Estilizando o header

---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo-purple.svg';
+
+import { Container, Content, Profile } from './styles.js';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>Douglas Nickson</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img src="" alt="Douglas Nickson" />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
